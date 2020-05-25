40180b27a53039c080c3291c87d0dc05aaddf201
Gilbert <gibbysmalls@icloud.com>
2020-05-24 20:35:47 -0400

    fix lightsail compose

--- lightsail-run.sh
+++ lightsail-run.sh
@@ -1,4 +1,4 @@
-curl -o lightsail-compose.sh https://raw.githubusercontent.com/TeplrGuy/augment/master/lightsail-compose.sh%20
+curl -o lightsail-compose.sh https://raw.githubusercontent.com/TeplrGuy/augment-fe/master/lightsail-compose.sh%20
 
 chmod +x ./lightsail-compose.sh
 
